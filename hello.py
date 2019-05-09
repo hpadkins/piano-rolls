6170c026de92f8a0e70aed2433a0754f9f1842f5
hpadkins <hadkins@pdx.edu>
2019-05-08 18:29:14 -0700

    fix line breaks

--- hello.py
+++ hello.py
@@ -2,20 +2,12 @@ import os
 from sqlalchemy import create_engine, MetaData, Table, Column, ForeignKey, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
-#from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, request, redirect, render_template
-#from models import db
+from flask import Flask, request, redirect, render_template, Markup
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-#from models import Book
-
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hadkins:Louise2007!@pianorolls.cnkqkcboxyt7.us-west-2.rds.amazonaws.com/pianorolls'
-#db = SQLAlchemy(app)
 
 
 Session = sessionmaker()
@@ -28,27 +20,22 @@ Base.prepare()
 Session.configure(bind=engine)
 session = Session()
 
-"""
-result = engine.execute("select title from songs")
-for row in result:
-	print("song: ", row['title'])
-result.close()
-#connection.close()
-"""
 
 Songs, Composers, Rolls = Base.classes.songs, Base.classes.composers, Base.classes.rolls
 
+"""
 songs = []
 for instance in session.query(Songs):
 	songs.append(instance.title)
 
 songsStr = ''.join(songs)
 print(songsStr)
+"""
 
 
 @app.route("/")
 def hello():
-	return render_template('hello.html', songs = songsStr)
+	return render_template('hello.html')
 
 @app.route("/", methods=["POST"])
 def hello_post():
@@ -63,10 +50,10 @@ def hello_post():
 		return "No results found for '"+titleSearch+"'"
 	else:
 		for song in query:
-			songs.append(song['title'])
+			songs.append(song['title'] + '<br>')
 
 		songsStr = ''.join(songs)
-		return songsStr
+		return Markup('<strong> '+songsStr +'</strong>')
 
 
 session.close()
